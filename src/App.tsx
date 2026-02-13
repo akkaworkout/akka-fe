1f30c439a5f01c37e57b10fbffd51ea325f9ca0e
tjsgpals <tjsgpals@tjsgpalsui-MacBookAir.local>
2026-02-14 00:28:04 +0900

    feat: 💄 마이페이지 UI 구현 및 메인 레이아웃 간격 정렬 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 import Main from './pages/main/Main'
 
-import WorkoutHistoryPage from "./pages/write/WorkoutHistoryPage";
-import ExpenseHistoryPage from "./pages/write/ExpenseHistoryPage";
-import TicketHistoryPage from './pages/write/TicketHistoryPage';
 
-import ReportPage from "./pages/report/ReportPage";
 
-import CalendarPage from "./pages/calendar/CalendarPage";
-
-import SignUpPage from "./pages/auth/SignUpPage";
-import LoginPage from "./pages/auth/LoginPage";
+import SignUpPage from './pages/auth/SignUpPage'
+import LoginPage from './pages/auth/LoginPage'
 import SignUpSuccessPage from './pages/auth/SignUpSuccessPage'
+
+import MyPage from './pages/mypage/MyPage'
 
 function App() {
   return (
@@ -20,23 +16,16 @@ function App() {
       <Route path="/main" element={<Main />} />
       <Route path="/" element={<Navigate to="/main" replace />} />
 
-      {/* 운동기록 */}
-      <Route path="/write" element={<WorkoutHistoryPage />} />
-      <Route path="/expense" element={<ExpenseHistoryPage />} />
-      <Route path="/ticket" element={<TicketHistoryPage />} />
-
-      {/* 분석/리포트 */}
-      <Route path="/report" element={<ReportPage />} />
-
-      {/* 캘린더 */}
-      <Route path="/calendar" element={<CalendarPage />} />
 
       {/* 로그인/회원가입 */}
       <Route path="/signup" element={<SignUpPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup/success" element={<SignUpSuccessPage />} />
+
+      {/* 마이페이지 */}
+      <Route path="/mypage" element={<MyPage />} />
     </Routes>
   )
 }
 
-export default App;
+export default App
